e61511e73017c6bdb7d7e1815a58ca11099b48a2
Sravan Kumar Thokala <sravan.kumar.thokala@intel.com>
2011-12-06 17:30:42 -0700

    Added Apache License 2.0 information for tnvme files

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Copyright (c) 2011, Intel Corporation.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
 RPMCOMPILEDIR=$1
 RPMSPECFILE=$2
 RPMSRCFILE=$3
@@ -25,8 +40,8 @@ if [ -z $RPMCOMPILEDIR ] || [ -z $RPMSPECFILE ] || [ -z $RPMSRCFILE ]; then
 fi
 
 # Setup a fresh RPM build environment, and then build
-MAJOR=`awk 'FNR == 5' version.h`
-MINOR=`awk 'FNR == 8' version.h`
+MAJOR=`awk 'FNR == 26' version.h`
+MINOR=`awk 'FNR == 29' version.h`
 rm -rf $RPMCOMPILEDIR
 mkdir -p $RPMCOMPILEDIR/{BUILDROOT,BUILD,RPMS,S{OURCE,PEC,RPM}S}
 cp -p $RPMSRCFILE.tar.gz  $RPMCOMPILEDIR
--- version.h
+++ version.h
@@ -1,6 +1,27 @@
+/*
+ * Copyright (c) 2011, Intel Corporation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 // Specify the software release version numbers on thier own line for use with
 // awk and the creation of RPM's while also being compatible with building
 // the binaries via the Makefile with *.cpp source code.
+
+// If the line numbers within this file change by the result of editing, then
+// you must modify both the Makefile and build.sh for awk parsing. Additionally
+// test this modification by running the Makefile rpm target.
+
 #define VER_MAJOR	\
 1
 
