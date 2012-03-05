7de38905d77432b61691425044e544a563507ff1
Todd Rentmeester <todd.rentmeester@intel.com>
2012-03-05 12:22:22 -0700

    Added test GrpNVMReadCmd::UnsupportRsvdField_r10b

---
+++ GrpNVMReadCmd/manySQtoCQAssoc_r10b.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2011, Intel Corporation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef _MANYSQTOCQASSOC_R10B_H_
+#define _MANYSQTOCQASSOC_R10B_H_
+
+#include <map>
+
+#include "test.h"
+#include "globals.h"
+#include "../Cmds/identify.h"
+#include "../Queues/iocq.h"
+#include "../Queues/iosq.h"
+#include "../Cmds/write.h"
+#include "../Utils/queues.h"
+
+namespace GrpQueues {
+
+
+/** \verbatim
+ * -----------------------------------------------------------------------------
+ * ----------------Mandatory rules for children to follow-----------------------
+ * -----------------------------------------------------------------------------
+ * 1) See notes in the header file of the Test base class
+ * \endverbatim
+ */
+class ManySQtoCQAssoc_r10b : public Test
+{
+public:
+    ManySQtoCQAssoc_r10b(int fd, string grpName, string testName,
+        ErrorRegs errRegs);
+    virtual ~ManySQtoCQAssoc_r10b();
+
+    /**
+     * IMPORTANT: Read Test::Clone() header comment.
+     */
+    virtual ManySQtoCQAssoc_r10b *Clone() const
+        { return new ManySQtoCQAssoc_r10b(*this); }
+    ManySQtoCQAssoc_r10b &operator=(const ManySQtoCQAssoc_r10b &other);
+    ManySQtoCQAssoc_r10b(const ManySQtoCQAssoc_r10b &other);
+
+
+protected:
+    virtual bool RunCoreTest();
+
+private:
+    ///////////////////////////////////////////////////////////////////////////
+    // Adding a member variable? Then edit the copy constructor and operator=().
+    ///////////////////////////////////////////////////////////////////////////
+    void SetWriteCmd(SharedWritePtr *writeCmd);
+    void ReapIOCQAndVerifyCE(SharedIOCQPtr iocq, uint16_t numTil,
+        vector<uint16_t> mSQIDToSQHDVector);
+};
+
+}   // namespace
+
+#endif
---
+++ GrpNVMReadCmd/unsupportRsvdFields_r10b.cpp
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2011, Intel Corporation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include "unsupportRsvdFields_r10b.h"
+#include "globals.h"
+#include "grpDefs.h"
+#include "../Queues/iocq.h"
+#include "../Queues/iosq.h"
+#include "../Utils/io.h"
+
+namespace GrpNVMReadCmd {
+
+
+UnsupportRsvdFields_r10b::UnsupportRsvdFields_r10b(int fd, string mGrpName, string mTestName,
+    ErrorRegs errRegs) :
+    Test(fd, mGrpName, mTestName, SPECREV_10b, errRegs)
+{
+    // 63 chars allowed:     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    mTestDesc.SetCompliance("revision 1.0b, section 6");
+    mTestDesc.SetShort(     "Set unsupported/rsvd fields in cmd");
+    // No string size limit for the long description
+    mTestDesc.SetLong(
+        "Search for 1 of the following namspcs to run test. Find 1st bare "
+        "namspc, or find 1st meta namspc, or find 1st E2E namspc. Unsupported "
+        "DW's and rsvd fields are treated identical, the recipient shall not "
+        "check their value. Issue a read cmd requesting 1 block and approp "
+        "supporting meta/E2E if necessary from the selected namspc at LBA 0, "
+        "expect success. Issue same cmd setting all unsupported/rsvd fields, "
+        "expect success. Set: DW0_b15:10, DW2, DW3, DW12_b25:16, DW13_b31:8");
+}
+
+
+UnsupportRsvdFields_r10b::~UnsupportRsvdFields_r10b()
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // Allocations taken from the heap and not under the control of the
+    // RsrcMngr need to be freed/deleted here.
+    ///////////////////////////////////////////////////////////////////////////
+}
+
+
+UnsupportRsvdFields_r10b::
+UnsupportRsvdFields_r10b(const UnsupportRsvdFields_r10b &other) : Test(other)
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // All pointers in this object must be NULL, never allow shallow or deep
+    // copies, see Test::Clone() header comment.
+    ///////////////////////////////////////////////////////////////////////////
+}
+
+
+UnsupportRsvdFields_r10b &
+UnsupportRsvdFields_r10b::operator=(const UnsupportRsvdFields_r10b &other)
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // All pointers in this object must be NULL, never allow shallow or deep
+    // copies, see Test::Clone() header comment.
+    ///////////////////////////////////////////////////////////////////////////
+    Test::operator=(other);
+    return *this;
+}
+
+
+bool
+UnsupportRsvdFields_r10b::RunCoreTest()
+{
+    /** \verbatim
+     * Assumptions:
+     * 1) Test CreateResources_r10b has run prior.
+     * \endverbatim
+     */
+    uint16_t numCE;
+    uint32_t isrCountB4;
+
+
+    // Lookup objs which were created in a prior test within group
+    SharedIOSQPtr iosq = CAST_TO_IOSQ(gRsrcMngr->GetObj(IOSQ_GROUP_ID));
+    SharedIOCQPtr iocq = CAST_TO_IOCQ(gRsrcMngr->GetObj(IOCQ_GROUP_ID));
+
+    if ((numCE = iocq->ReapInquiry(isrCountB4, true)) != 0) {
+        LOG_ERR("Require 0 CE's within CQ %d, not upheld, found %d",
+            iocq->GetQId(), numCE);
+        throw exception();
+    }
+
+    SharedReadPtr readCmd = CreateCmd();
+    IO::SendCmdToHdw(mGrpName, mTestName, DEFAULT_CMD_WAIT_ms, iosq, iocq,
+        readCmd, "none.set", true);
+
+    LOG_NRM("Set all cmd's rsvd bits");
+    uint32_t work = readCmd->GetDword(0);
+    work |= 0x0000fc00;      // Set DW0_b15:10 bits
+    readCmd->SetDword(work, 0);
+
+    readCmd->SetDword(0xffff, 2);
+    readCmd->SetDword(0xffff, 3);
+
+    work = readCmd->GetDword(12);
+    work |= 0x03ff0000;      // Set DW12_b25:16 bits
+    readCmd->SetDword(work, 12);
+
+    work = readCmd->GetDword(13);
+    work |= 0xffffff00;     // Set DW13_b31:8 bits
+    readCmd->SetDword(work, 13);
+
+    IO::SendCmdToHdw(mGrpName, mTestName, DEFAULT_CMD_WAIT_ms, iosq, iocq,
+        readCmd, "all.set", true);
+
+    return true;
+}
+
+
+SharedReadPtr
+UnsupportRsvdFields_r10b::CreateCmd()
+{
+    SharedReadPtr readCmd = Read::NullReadPtr;
+
+
+    Informative::Namspc namspcData = gInformative->Get1stBareMetaE2E();
+    if (namspcData.type != Informative::NS_BARE) {
+        LBAFormat lbaFormat = namspcData.idCmdNamspc->GetLBAFormat();
+        if (gRsrcMngr->SetMetaAllocSize(lbaFormat.MS) == false)
+            throw exception();
+    }
+    LOG_NRM("Processing read cmd using namspc id %d", namspcData.id);
+
+    ConstSharedIdentifyPtr namSpcPtr = namspcData.idCmdNamspc;
+    uint64_t lbaDataSize = namSpcPtr->GetLBADataSize();;
+    SharedMemBufferPtr dataPat = SharedMemBufferPtr(new MemBuffer());
+    dataPat->Init(lbaDataSize);
+
+    readCmd = SharedReadPtr(new Read(mFd));
+    send_64b_bitmask prpBitmask = (send_64b_bitmask)(MASK_PRP1_PAGE
+        | MASK_PRP2_PAGE | MASK_PRP2_LIST);
+
+    if (namspcData.type == Informative::NS_META) {
+        readCmd->AllocMetaBuffer();
+        prpBitmask = (send_64b_bitmask)(prpBitmask | MASK_MPTR);
+    } else if (namspcData.type == Informative::NS_E2E) {
+        readCmd->AllocMetaBuffer();
+        prpBitmask = (send_64b_bitmask)(prpBitmask | MASK_MPTR);
+        LOG_ERR("Deferring E2E namspc work to the future");
+        LOG_ERR("Need to add CRC's to correlate to buf pattern");
+        throw exception();
+    }
+
+    readCmd->SetPrpBuffer(prpBitmask, dataPat);
+    readCmd->SetNSID(namspcData.id);
+    readCmd->SetNLB(0);
+    return readCmd;
+}
+
+
+}   // namespace
+
