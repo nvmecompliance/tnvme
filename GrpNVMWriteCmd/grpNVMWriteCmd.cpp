9559e96ec1069a8c0ba6f2d8c518757e6577f456
Todd Rentmeester <todd.rentmeester@intel.com>
2012-03-19 10:06:12 -0600

    Added test case GrpNVMWriteCmd/ignoreMetaPtrBase_r10b

--- GrpNVMWriteCmd/grpNVMWriteCmd.cpp
+++ GrpNVMWriteCmd/grpNVMWriteCmd.cpp
@@ -19,7 +19,7 @@
 #include "lbaOutOfRangeBare_r10b.h"
 #include "invalidNamspc_r10b.h"
 #include "unsupportRsvdFields_r10b.h"
-//todo#include "ignoreMetaPtrBase_r10b.h"
+#include "ignoreMetaPtrBase_r10b.h"
 //todo#include "FUA_r10b.h"
 //todo#include "limitedRetry_r10b.h"
 
@@ -41,12 +41,8 @@ GrpNVMWriteCmd::GrpNVMWriteCmd(size_t grpNum, SpecRev specRev,
         APPEND_TEST_AT_XLEVEL(CreateResources_r10b, fd, GrpNVMWriteCmd, errRegs)
         APPEND_TEST_AT_YLEVEL(LBAOutOfRangeBare_r10b, fd, GrpNVMWriteCmd, errRegs)
         APPEND_TEST_AT_YLEVEL(InvalidNamspc_r10b, fd, GrpNVMWriteCmd, errRegs)
-<<<<<<< HEAD
         APPEND_TEST_AT_YLEVEL(UnsupportRsvdFields_r10b, fd, GrpNVMWriteCmd, errRegs)
-=======
-//todo        APPEND_TEST_AT_YLEVEL(UnsupportRsvdFields_r10b, fd, GrpNVMWriteCmd, errRegs)
->>>>>>> newTest
-//todo        APPEND_TEST_AT_YLEVEL(IgnoreMetaPtrBase_r10b, fd, GrpNVMWriteCmd, errRegs)
+        APPEND_TEST_AT_YLEVEL(IgnoreMetaPtrBase_r10b, fd, GrpNVMWriteCmd, errRegs)
 //todo        APPEND_TEST_AT_YLEVEL(FUA_r10b, fd, GrpNVMWriteCmd, errRegs)
 //todo        APPEND_TEST_AT_YLEVEL(LimitedRetry_r10b, fd, GrpNVMWriteCmd, errRegs)
         break;
