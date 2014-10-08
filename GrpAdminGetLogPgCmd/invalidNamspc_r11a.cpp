6567f4edf751a10363fd800917d4a11c6d2464b3
Mike Bogochow <mbogochow@hotmail.com>
2014-10-08 10:55:54 -0400

    Fixed the GrpAdminLogPgCmd InvalidNamspc_r10b test to run correctly and also made the namespace id to logs print more reader friendly (in hex).  Implemented new test invalidNamspc_r11a which implements functionality of this test for the spec revision 1.1a.

--- GrpAdminGetLogPgCmd/invalidNamspc_r11a.cpp
+++ GrpAdminGetLogPgCmd/invalidNamspc_r11a.cpp
@@ -15,7 +15,7 @@
  */
 
 #include <boost/format.hpp>
-#include "invalidNamspc_r10b.h"
+#include "invalidNamspc_r11a.h"
 #include "globals.h"
 #include "grpDefs.h"
 #include "../Utils/kernelAPI.h"
@@ -28,23 +28,26 @@
 namespace GrpAdminGetLogPgCmd {
 
 
-InvalidNamspc_r10b::InvalidNamspc_r10b
-    (string grpName, string testName) : Test(grpName, testName, SPECREV_10b)
+InvalidNamspc_r11a::InvalidNamspc_r11a
+    (string grpName, string testName) : Test(grpName, testName, SPECREV_10b
+        /*SPECREV_11a*/)
 {
     // 63 chars allowed:     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    mTestDesc.SetCompliance("revision 1.0b, section 5");
+    mTestDesc.SetCompliance("revision 1.1a, section 5.10");
     mTestDesc.SetShort(     "Issue GetLogPage and cause SC=Invalid Namspc or Format");
     // No string size limit for the long description
     mTestDesc.SetLong(
         "Issue GetLogPage cmd, LID=2, NUMD=(512/4). Determine Identify.LPA.0, "
-        "and if SMART is supported, then determine X=Identity.NN and loop "
-        "GetLogPage to all namspcs not supported by DUT and expect failure, "
-        "0xFFFFFFFF is legal and return global page. If SMART "
-        "is not supported then all NSID values are legal, verify success.");
+        "and if SMART is supported on a per namespace basis, then determine "
+        "X=Identity.NN and loop GetLogPage to all namspcs not supported by DUT "
+        "and expect failure, 0xFFFFFFFF is legal and return global page. If "
+        "SMART is not supported one a per namespace basis, then any NSID value "
+        "other than 0xFFFFFFFF is invalid and should abort with status Invalid "
+        "Field in Command.");
 }
 
 
-InvalidNamspc_r10b::~InvalidNamspc_r10b()
+InvalidNamspc_r11a::~InvalidNamspc_r11a()
 {
     ///////////////////////////////////////////////////////////////////////////
     // Allocations taken from the heap and not under the control of the
@@ -53,8 +56,8 @@ InvalidNamspc_r10b::~InvalidNamspc_r10b()
 }
 
 
-InvalidNamspc_r10b::
-InvalidNamspc_r10b(const InvalidNamspc_r10b &other) : Test(other)
+InvalidNamspc_r11a::
+InvalidNamspc_r11a(const InvalidNamspc_r11a &other) : Test(other)
 {
     ///////////////////////////////////////////////////////////////////////////
     // All pointers in this object must be NULL, never allow shallow or deep
@@ -63,8 +66,8 @@ InvalidNamspc_r10b(const InvalidNamspc_r10b &other) : Test(other)
 }
 
 
-InvalidNamspc_r10b &
-InvalidNamspc_r10b::operator=(const InvalidNamspc_r10b &other)
+InvalidNamspc_r11a &
+InvalidNamspc_r11a::operator=(const InvalidNamspc_r11a &other)
 {
     ///////////////////////////////////////////////////////////////////////////
     // All pointers in this object must be NULL, never allow shallow or deep
@@ -76,7 +79,7 @@ InvalidNamspc_r10b::operator=(const InvalidNamspc_r10b &other)
 
 
 Test::RunType
-InvalidNamspc_r10b::RunnableCoreTest(bool preserve)
+InvalidNamspc_r11a::RunnableCoreTest(bool preserve)
 {
     ///////////////////////////////////////////////////////////////////////////
     // All code contained herein must never permanently modify the state or
@@ -90,7 +93,7 @@ InvalidNamspc_r10b::RunnableCoreTest(bool preserve)
 
 
 void
-InvalidNamspc_r10b::RunCoreTest()
+InvalidNamspc_r11a::RunCoreTest()
 {
     /** \verbatim
      * Assumptions:
@@ -121,7 +124,6 @@ InvalidNamspc_r10b::RunCoreTest()
         throw FrmwkEx(HERE, "Required to support >= 1 namespace");
 
     uint8_t logPageAttr = (uint8_t)idCtrlrStruct->GetValue(IDCTRLRCAP_LPA);
-    // bits 7:1 are reserved, need bit 0
     uint8_t suppSmartPerNamspc = logPageAttr & 0x1;
 
     // for all illegal nsid's verify get log page cmd.
@@ -133,8 +135,8 @@ InvalidNamspc_r10b::RunCoreTest()
         work = str(boost::format("namspc%d") % i);
         // Determine the status based on the per namespace mask bit.
         IO::SendAndReapCmd(mGrpName, mTestName, CALC_TIMEOUT_ms(1), asq, acq,
-            getLogPgCmd, work, true, suppSmartPerNamspc ? CESTAT_INVAL_NAMSPC :
-                CESTAT_SUCCESS);
+            getLogPgCmd, work, true, suppSmartPerNamspc ? CESTAT_INVAL_NAMSPC : 
+            CESTAT_INVAL_FIELD);
     }
 
     // If per namespc smart log is supported, check all legal nn.
@@ -149,15 +151,11 @@ InvalidNamspc_r10b::RunCoreTest()
             IO::SendAndReapCmd(mGrpName, mTestName, CALC_TIMEOUT_ms(1),
                 asq, acq, getLogPgCmd, work, true);
         }
-    } else {
-        i = 0;
-        getLogPgCmd->SetNSID(i);
-        work = str(boost::format("namspc%d") % i);
-        IO::SendAndReapCmd(mGrpName, mTestName, CALC_TIMEOUT_ms(1), asq, acq,
-            getLogPgCmd, work, true);
     }
 
     // If name space is 0xffffffff, then we should get the global log info.
+    // The SMART / Health info log page shall be supported on a global basis
+    // regardless of whether it is supported on a per namespace basis as well.
     i = 0xffffffff;
     getLogPgCmd->SetNSID(i);
     work = str(boost::format("namspc%d") % i);
@@ -167,3 +165,4 @@ InvalidNamspc_r10b::RunCoreTest()
 
 
 }   // namespace
+
